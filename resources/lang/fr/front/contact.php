166b20fc798ed984b8047839579d1750b749fab7
soywod <clementdouin21@gmail.com>
2016-01-29 10:34:00 +0100

    Remove : in mail template

--- resources/lang/fr/front/contact.php
+++ resources/lang/fr/front/contact.php
@@ -3,6 +3,6 @@
 return [
 	'success_sent'        => 'email envoyé avec succès !',
 	'new_message_subject' => 'nouveau message depuis helene-gidel-soto.com',
-	'new_message'         => 'vous avez reçu une nouveau message de la part de Mr/Mme :name : ',
+	'new_message'         => 'vous avez reçu une nouveau message de la part de Mr/Mme :name',
 	'captcha_failed'      => 'erreur lors de la validation du captcha',
 ];
